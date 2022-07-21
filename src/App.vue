599d40a564038ada79cc2325f973561c764aafbd
a.sivak <riziy_bes@mail.ru>
2022-07-21 23:30:45 +0200

    add background image

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,16 @@
     </div>
 </template>
 
-<style></style>
+<style scoped>
+.main {
+    background-image: linear-gradient(
+            180deg,
+            rgba(0, 0, 0, 0.2),
+            rgba(0, 0, 0, 0.85)
+        ),
+        url('~@/assets/img/bg.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+</style>
